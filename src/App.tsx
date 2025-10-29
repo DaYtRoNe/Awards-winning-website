0ddcca6c75359f19f20eb3b00cd2e36227df180a
Eshan <eshangunsekara@gmail.com>
2025-10-29 16:50:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">App</h1>
+    </div>
+  )
+}
+
+export default App
